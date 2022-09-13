ad9e1081c3eab50595f630d1d7f6d78e159ee45d
ArutyunyanAyk <haykharutyunyan38@gmail.com>
2022-09-14 00:24:34 +0300

    template

--- Project1/Project1/Source.cpp
+++ Project1/Project1/Source.cpp
@@ -7,40 +7,48 @@ template <class Type> std::ostream& operator <<(std::ostream& out, const TPair<T
 
 template <class Type>
 class TPair {
-	Type first;
-	Type second;
+	Type x;
+	Type y;
 public:
-	TPair() : first(0), second(0) {}
+	TPair()
+	{
+		x = 0;
+		y = 0;
+	}
 
-	void veryImportantFunc();
+	TPair(Type x1, Type y1)
+	{
+		x = x1;
+		y = y1;
+	}
 
-	TPair operator +<Type>(const TPair& right)
+	TPair operator +(const TPair& right)
 	{
 		TPair coord;
 
-		coord.first = this->firstx + right.first
-		coord.second = this->second + right.second;
+		coord.x = this->x + right.x;
+		coord.y = this->y + right.y;
 
 		return coord;
 	}
 
-	TPair <Type> operator =(const TPair& right)
+	TPair operator =(const TPair& right)
+	{
+		if (this == &right)
 		{
-			if (this == &right) 
-			{
-				return *this;
-			}
-	
-			first = right.first;
-			second = right.second;
-	
-			return *this;
+			return this;
 		}
+
+		x = right.x;
+		y = right.y;
+
+		return this;
+	}
+
+	void veryImportantFunc();
 
 	friend std::ostream& operator<< <Type>(std::ostream& out, const TPair& pair);
 };
-
-
 
 template <class Type>
 void TPair<Type>::veryImportantFunc() {
@@ -49,13 +57,12 @@ void TPair<Type>::veryImportantFunc() {
 
 template <class Type>
 std::ostream& operator <<(std::ostream& out, const TPair<Type>& pair) {
-	out << "(" << pair.first << ", " << pair.second << ")" << std::endl;
+	out << "(" << pair.x << ", " << pair.y << ")" << std::endl;
 	return out;
 }
 
 int main() {
 	TPair<int> coords;
-
 	coords.veryImportantFunc();
 	std::cout << coords << std::endl;
 
